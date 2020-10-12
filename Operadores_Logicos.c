4f6edcb5538e5a22857c3d617a605cbdce66a8a5
marcoswgallo <70547583+marcoswgallo@users.noreply.github.com>
2020-10-11 21:50:39 -0300

    Add files via upload

---
+++ Operadores_Logicos.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    int iJ, iI;
+    iJ = 17;
+    iI = 60;
+
+    int idade = 0;
+
+    printf("Digite a idade de uma pessoa:\n");
+    scanf("%i", &idade);
+
+    if(idade<=iJ)
+    {
+        printf("\nA idade informada e de um jovem.\n");
+    }else{
+        if(idade>=iI){
+            printf("\nA idade informada e de uma pessoa idosa.\n");
+        }else{
+            if((idade > iJ)&&(idade < iI));
+            printf("A idade informada e de uma pessoa de meia-idade.");
+        }
+    }
+    
+
+    return 0;
+}
---
+++ Tomada_Decisoes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 10;
+
+    if(5!=5) //COMPARAÇÃO
+    {
+
+    }else{
+        
+    }
+    
+    return 0;
+
+}
